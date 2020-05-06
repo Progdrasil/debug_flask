fe42ffc43128fff27dd85a55786d509f063d9954
Rene Leveille <rene.leveille@polymtl.ca>
2020-05-06 10:31:17 -0400

    able to reproduce

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=debug_flask
+FLASK_ENV=development
+FLASK_DEBUG=1
--- debug_flask/__init__.py
+++ debug_flask/__init__.py
@@ -1,1 +1,19 @@
-__version__ = '0.1.0'
+from flask import Flask
+
+__version__ = "0.1.0"
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_debug():
+    return {"Hello": "Debug"}
+
+
+@app.route("/test-reload")
+def reload():
+    return {"reloading": "should not work"}
+
+
+@app.route("test")
+def test():
+    return "test"
